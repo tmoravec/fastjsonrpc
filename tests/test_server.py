104ed80882bbf1900f9a68e927b76808a6aa2300
Tadeas Moravec <belgarath@linux-oy3l.site>
2012-08-13 20:41:23 +0200

    Remove stale comments

--- tests/test_server.py
+++ tests/test_server.py
@@ -11,17 +11,6 @@ from twisted.internet.defer import succeed
 
 from fastjsonrpc.server import JSONRPCServer
 
-# two types: 1) start server and use client, 2) test render(...) directly
-#
-# 1) a) prepare methods to test
-#    b) start on random port in setUp
-#    c) test via (async) client
-#    d) stop in tearDown
-#
-# 2) a) mock Request object-.content, .content.seek, .write, .finish, .setHeader
-#    b) test render() directly
-#    c) prepare various JSONs
-#
 
 def _render(resource, request):
     result = resource.render(request)
