7d82c0b3969eefd36d5a27939de0e5b65c36ef54
Martin Rutar <martin.rutar@nodus.cz>
2012-10-05 10:02:06 +0000

    setup.py for distutils added

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import distutils.core
+
+distutils.core.setup(
+    name = "fastjsonrpc",
+    version = "0.1",
+    packages = ["fastjsonrpc"],
+    author = "Tadeas Moravec",
+    author_email = "tadeas.moravec@email.cz",
+    url = "http://github.com/tadeas/fastjsonrpc",
+    license = "http://www.apache.org/licenses/LICENSE-2.0",
+    description = "A library for writing asynchronous JSON-RPC servers and clients in Python, using Twisted.",
+    long_description = open("README.md").read()
+)
