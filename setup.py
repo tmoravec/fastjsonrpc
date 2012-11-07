07c57219269540e1657442390b326fca699780e0
Tadeas Moravec <tadeas@silvanest.site>
2012-11-07 23:41:50 +0100

    Attempt to persuade PyPI to accept readme in a file

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ limitations under the License.
 
 import distutils.core
 
+with open("README.md") as readme:
+    long_description = readme.read()
+
 distutils.core.setup(
     name = "fastjsonrpc",
     version = "0.1",
@@ -24,7 +27,7 @@ distutils.core.setup(
     url = "http://github.com/tadeas/fastjsonrpc",
     license = "http://www.apache.org/licenses/LICENSE-2.0",
     description = "A library for writing asynchronous JSON-RPC servers and clients in Python, using Twisted.",
-    long_description = open("README.md").read(),
+    long_description = long_description,
     classifiers = [
         'Development Status :: 3 - Alpha',
         ]
