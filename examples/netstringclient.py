691889013111f402417edeb3ee5e4fda083a40e7
Tadeas Moravec <tadeas@silvanest.site>
2013-01-06 01:07:04 +0100

    Better representation of errors

--- examples/netstringclient.py
+++ examples/netstringclient.py
@@ -7,12 +7,18 @@ from twisted.internet import reactor
 from fastjsonrpc.netstringclient import Proxy
 
 
+def printError(error):
+    print 'error:', error.value
+    reactor.stop()
+
+
 def printResult(result):
     print 'response:', result
     reactor.stop()
 
 proxy = Proxy('127.0.0.1:8999', verbose=True)
-d = proxy.callRemote('echo', 'abc')
-d.addBoth(printResult)
+d = proxy.callRemote('none')
+d.addCallback(printResult)
+d.addErrback(printError)
 
 reactor.run()
