88aab00c46572ee9573c9d9b6baec0395da4d03a
Tadeas Moravec <tadeas@silvanest.site>
2013-02-26 17:48:17 +0100

    Call two methods in netstring example (triggers an error...)

--- examples/netstringclient.py
+++ examples/netstringclient.py
@@ -3,25 +3,39 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 from twisted.internet import reactor
+from twisted.internet import defer
 from twisted.python import log
 
 from fastjsonrpc.netstringclient import Proxy
 
 
+def printValue(value):
+    print "Result: %s" % str(value)
+
+
 def printError(error):
-    print 'error:', error.value
-    reactor.stop()
+    print 'error', error.value
 
 
-def printResult(result):
-    print 'response:', result
+def shutDown(data):
+    print "Shutting down reactor..."
     reactor.stop()
 
 
 log.startLogging(sys.stdout)
+
+dl = []
 proxy = Proxy('127.0.0.1:8999', verbose=True)
+
 d = proxy.callRemote('none')
-d.addCallback(printResult)
-d.addErrback(printError)
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+d = proxy.callRemote('none')
+d.addCallbacks(printValue, printError)
+dl.append(d)
+
+dl = defer.DeferredList(dl)
+dl.addCallback(shutDown)
 
 reactor.run()
