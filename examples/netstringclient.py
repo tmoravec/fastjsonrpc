f2779f5479af480a20afc5fce5eca01e86713697
Tadeas Moravec <tadeas@silvanest.site>
2013-02-26 17:47:59 +0100

    Enable logging in example

--- examples/netstringclient.py
+++ examples/netstringclient.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 from twisted.internet import reactor
+from twisted.python import log
 
 from fastjsonrpc.netstringclient import Proxy
 
@@ -16,6 +17,8 @@ def printResult(result):
     print 'response:', result
     reactor.stop()
 
+
+log.startLogging(sys.stdout)
 proxy = Proxy('127.0.0.1:8999', verbose=True)
 d = proxy.callRemote('none')
 d.addCallback(printResult)
