f3f712d2da66685236d29288c3ec11db89427d35
Tadeas Moravec <tadeas@linux-xrqj.site>
2013-04-01 16:45:07 +0200

    Basic tests of netstring server

---
+++ tests/dummynetstringserver.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+from fastjsonrpc.netstringserver import JSONRPCServer
+
+
+class DummyProtocol(JSONRPCServer):
+
+    def __init__(self):
+        JSONRPCServer.__init__(self, True)
+
+    def jsonrpc_echo(self, data):
+        return data
+
+    def jsonrpc_none(self):
+        raise Exception('vyjimka')
+
+    def jsonrpc_add(self, a, b):
+        return a + b
+
+    def jsonrpc_mysql_first_user(self):
+        def capitalize(sql_result):
+            if sql_result:
+                return sql_result[0][0].upper()
+            else:
+                return None
+
+        sql = 'SELECT User FROM user LIMIT 1'
+        dbpool = adbapi.ConnectionPool('MySQLdb', 'localhost', user='root',
+                                       passwd='', db='mysql')
+        d = dbpool.runQuery(sql)
+        d.addCallback(capitalize)
+        return d
